ff841049f00c0a0bd784f013bb4e0761054b031c
zhafrana <mz.arman@gmail.com>
2023-10-24 17:47:21 +0700

    Configuring Project Structure

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -3,10 +3,16 @@ import Layout from "../../components/Layout";
 Layout
 const Home = () => {
   return (
-    <> 
-      This is Homepage
+    <>
+      <section className="container">
+        <div className="hero-section">
+          <h1 className="hero-text">
+          Wilujeng Tepang, I&apos;m Muhammad Zhafran Arman, a passionate frontend developer and UI/UX Designer from Bandung, Indonesia. I have an obsession with design and the possibilities of human-computer interaction. So I spend most of my time crafting delightful digital experiences.
+          </h1>
+        </div>
+      </section>
     </>
-   );
+  );
 }
- 
+
 export default Home;
