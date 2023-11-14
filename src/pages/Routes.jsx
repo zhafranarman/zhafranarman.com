87852c4b74e61d50b81070d6bb0ecbe694eccd7b
zhafrana <mz.arman@gmail.com>
2023-11-14 14:31:38 +0700

    Fixing Deployment

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "./home/index";
+import Home from "./home/Index.jsx";
 
 const RoutesList = () => {
 
