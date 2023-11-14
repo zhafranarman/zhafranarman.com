62df2126b8f4eae698111abae8dfb0024a213d8a
zhafrana <mz.arman@gmail.com>
2023-11-14 20:58:55 +0700

    Cleaning Up Animation

--- src/pages/home/aboutSection.jsx
+++ src/pages/home/aboutSection.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react"
 import { motion, useInView } from "framer-motion"
-import about_img from "../../assets/images/about-rizz.webp"
+import about_img from "../../assets/images/about.webp"
+import about_img2 from "../../assets/images/about2.webp"
 
 const AboutSection = () => {
 
@@ -12,13 +13,21 @@ const AboutSection = () => {
     visible: (i) => ({
       y: "0%",
       opacity: 1,
-      transition: { duration: 0.5, delay: 0.015 * i }
+      transition: { duration: 0.5, delay: 0.02 * i }
+    }),
+  }
+  const wordAni2 = {
+    hidden: { y: "100%", opacity: 0, transition: { duration: 0.5 } },
+    visible: (i) => ({
+      y: "0%",
+      opacity: 1,
+      transition: { duration: 1, delay: 0.02 * i }
     }),
   }
 
   const titleAni = {
     hidden: {
-      y: "100%",
+      y: "30%",
       opacity: 0
     },
     visible: {
@@ -29,32 +38,25 @@ const AboutSection = () => {
   }
   const ctaAni = {
     hidden: {
-      y: "100%",
+      y: "20%",
       opacity: 0
     },
     visible: {
       y: "0",
       opacity: 1,
-      transition: { duration: 0.5, delay: 0.4 },
+      transition: { duration: 1, delay: 0.5 },
     }
   }
 
-  const phrase = "During my teenage years, I was fascinated by how technology works, and the potential for human-computer interaction always amazed me. Now, I spend most of my time creating digital experiences for people."
-  const phrase2 = "I am currently freelancing and on the lookout for exciting new opportunities, especially in the realms of design and frontend development. Ready to embark on the next chapter of creativity and innovation!"
+  const phrase = `During my teenage years, I was fascinated by how technology works, and the potential for human-computer interaction always amazed me. Now, I spend most of my time creating digital experiences for people.`
+  const phrase2 = `I am currently freelancing and on the lookout for exciting new opportunities, especially in the realms of design and frontend development. Ready to embark on the next chapter of creativity and innovation!`
+
 
   return (
     <section className="about-section container">
       <div data-scroll data-scroll-section className="about-content inner-section" ref={content}>
-        <div className="about-heading">
+        <div className="about-column">
           <motion.h2 variants={titleAni} initial="hidden" animate={isInView ? "visible" : "hidden"} className="about-title"><span className="about-title-row">Here’s the</span><span className="about-title-row">tl;dr on me</span></motion.h2>
-          <figure data-scroll data-scroll-speed="0.15" data-scroll-position="end, start" className="about-image-frame is-inview">
-            <img className="about-image" src={about_img} />
-            <figcaption className="about-image-caption">
-              Sometimes, I dig <br /> working in a chill place.
-            </figcaption>
-          </figure>
-        </div>
-        <div>
           <p className="about-text">
             {
               phrase.split(" ").map((word, index) => {
@@ -65,14 +67,33 @@ const AboutSection = () => {
           <p className="about-text">
             {
               phrase2.split(" ").map((word, index) => {
-                return <span key={index} className="word-mask"><motion.span className="word" variants={wordAni} initial="hidden" animate={isInView ? "visible" : "hidden"} custom={index}>{word}&nbsp;</motion.span></span>
+                return <span key={index} className="word-mask"><motion.span className="word" variants={wordAni2} initial="hidden" animate={isInView ? "visible" : "hidden"} custom={index}>{word}&nbsp;</motion.span></span>
               })
             }
           </p>
         </div>
-        {/* <motion.a variants={ctaAni} initial="hidden" animate={isInView ? "visible" : "hidden"} className="about-me-cta" href="#">
-            Read My Story
-          </motion.a> */}
+        <div className="about-column">
+          <figure data-scroll data-scroll-speed="0.15" className="about-image-frame is-inview">
+            <img className="about-image" src={about_img2} />
+            <figcaption className="about-image-caption">
+              Me and my friends, working on self initiated project.
+            </figcaption>
+          </figure>
+          <figure data-scroll data-scroll-speed="0.1" className="about-image-frame is-inview">
+            <img className="about-image" src={about_img} />
+            <figcaption className="about-image-caption">
+              Sometimes, I dig <br /> working in a chill place.
+            </figcaption>
+          </figure>
+        </div>
+        <motion.div className="about-cta" variants={ctaAni} initial="hidden" animate={isInView ? "visible" : "hidden"} >
+          <a className="about-cta-text" href="#">
+            Read More About <br/> My Story
+          </a>
+          <svg width="154" height="53" viewBox="0 0 154 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M152.501 1.49756C140 40 109.5 57 69.9999 49.5C34.3565 42.7323 1.09785 6.56696 0.99986 6.5M0.99986 6.5C0.901866 6.43304 11.4999 3.5 21.4999 4.5M0.99986 6.5C0.901866 6.43304 0.999891 28 0.999891 28" stroke="white" strokeWidth="1.75" strokeLinecap="round" />
+          </svg>
+        </motion.div>
       </div>
     </section>
   );
