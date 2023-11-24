2553aac4ca9a16e1ff4aa3c49bd91b8f3165a436
zhafrana <mz.arman@gmail.com>
2023-11-24 23:59:22 +0700

    Discover Bug Routing

--- src/pages/about/AboutContent.jsx
+++ src/pages/about/AboutContent.jsx
@@ -1,18 +1,21 @@
 const AboutContent = () => {
   return (
     <section className="container">
-      <div className="inner-section about-content-inner-section">
+      <div className="inner-section">
         <div className="about-content-paragraph">
           <p>
             Before I stepped into the world of UI/UX Design,I started my journey as a graphic designer. One of my initial job involved crafting designs for shirts, posters, and social media posts, a time when designer apps didn't offer an abundance of free templates. It was an enjoyable experience, though vastly different from my college department.
           </p>
           <p>
-            The turning point in my design trajectory occurred when friends introduced me to the fascinating world of UI/UX Design. I found myself deeply enamored with the seamless blend of design principles and technological innovation. This newfound passion led me to become not only a frontend developer but also a dedicated UI/UX Designer, a dual role that I've diligently nurtured and refined over the years.
+            The turning point in my design trajectory occurred when my friends introduced me to the fascinating world of UI/UX Design. I found myself deeply enamored with the seamless blend of design principles and technological innovation. This newfound passion led me to become not only a frontend developer but also a dedicated UI/UX Designer, a dual role that I've diligently nurtured and refined over the years.
           </p>
           <p>
-            Beyond the digital canvas, my life takes on vibrant hues. I relish moments spent sipping a comforting cup of coffee, the rhythmic joy of both listening to and playing music, and embarking on adventures that unfold the diverse landscapes of our world alongside my partner."
+            Beyond the digital canvas, i enjoy a good cup of coffee, grooving to music—sometimes even playing some tunes myself. And I love spending time with my friends and family and go on spontaneous adventures. Just everyday stuff, you know?
           </p>
         </div>
+      </div>
+      <div className="inner-section">
+
         <article className="experience">
           <div className="experience-column">
             Experience
@@ -32,7 +35,7 @@ const AboutContent = () => {
             <div className="experience-items">
               <span className="experience-title">Katajihiji, Indococotama</span>
               <span className="experience-time">( 2021 - 2023)</span>
-              <p className="experience-desc">At katajihiji.id and indococotama, both under the same ownership, my responsibility are, to create of an engaging landing page. Additionally, I managed the setup of their corporate email and server configuration, blending design finesse with technical optimization."</p>
+              <p className="experience-desc">At katajihiji.id and indococotama, both under the same ownership, my responsibilies were, to create of an engaging landing page. Additionally, I managed the setup of their corporate email and server configuration, blending design finesse with technical optimization."</p>
             </div>
             <div className="experience-items">
               <span className="experience-title">Akselerasi Entertaintment </span>
@@ -41,6 +44,8 @@ const AboutContent = () => {
             </div>
           </div>
         </article>
+      </div>
+      <div className="inner-section">
         <article className="education">
           <div className="education-column">
             Education
@@ -48,7 +53,7 @@ const AboutContent = () => {
           <div className="education-column">
             <div className="education-items">
               <span className="education-title">Institut Teknologi Nasional</span>
-              <span className="education-time">(Bachelor's of Information System)</span>
+              <span className="education-time">(Bachelor of Information System)</span>
               <p className="education-desc">During my college years, my academic focus was on Information Systems. This dynamic field taught me the art of blending business with technology, emphasizing the creation of enterprise architecture and the design of holistic systems. Beyond textbooks, I gained hands-on experience in applying these concepts to craft innovative solutions at the intersection of business and technology.</p>
             </div>
           </div>
