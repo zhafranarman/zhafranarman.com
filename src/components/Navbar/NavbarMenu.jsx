3159e4b9061c7f430141872197364e6a3a2ab57d
zhafrana <mz.arman@gmail.com>
2023-11-30 09:10:44 +0700

    Navbar Finalization

--- src/components/Navbar/NavbarMenu.jsx
+++ src/components/Navbar/NavbarMenu.jsx
@@ -1,10 +1,13 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const NavbarMenu = () => {
+  const location = useLocation();
+  const { pathname } = location;
+  
   return (
     <ul className="nav-menu">
-      <li><NavLink className="nav-links" to="/about">About</NavLink></li>
-      <li><NavLink className="nav-links" to="/work">Work</NavLink></li>
+      <li><NavLink className={`nav-links ${pathname === "/about" ? 'active' : ''}`} to="/about">About</NavLink></li>
+      <li><NavLink className={`nav-links ${pathname === "/work" ? 'active' : ''}`}to="/work">Work</NavLink></li>
       <li>
         <a className="nav-links" href="mailto:hi@zhafranarman.com">Open to Work</a>
       </li>
