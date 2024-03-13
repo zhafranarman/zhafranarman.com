75da18281818a303886f320b3087cebd93621fbd
zhafrana <mz.arman@gmail.com>
2024-03-14 03:19:01 +0700

    Fixing Plausible Script

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,21 @@ import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
+import { PlausibleProvider, Plausible } from 'next-plausible';
 
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <App />
-        <SpeedInsights />
-        <Analytics />
-      </BrowserRouter>
+      <PlausibleProvider domain="zhafranarman.com" customDomain="https://analytics.zhafran.dev">
+        <BrowserRouter>
+          <App />
+          <SpeedInsights />
+          <Analytics />
+          <Plausible />
+        </BrowserRouter>
+      </PlausibleProvider>
     </HelmetProvider>
   </React.StrictMode>,
 )
