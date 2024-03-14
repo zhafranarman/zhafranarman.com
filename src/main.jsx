d9321a2f83ea9b352ea8972837542d61ad8afa89
zhafrana <mz.arman@gmail.com>
2024-03-14 14:09:56 +0700

    Fixing Plausible Script Attempt 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
-import { PlausibleProvider, Plausible } from 'next-plausible';
-
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <PlausibleProvider domain="zhafranarman.com" customDomain="https://analytics.zhafran.dev">
-        <BrowserRouter>
-          <App />
-          <SpeedInsights />
-          <Analytics />
-          <Plausible />
-        </BrowserRouter>
-      </PlausibleProvider>
+      <Helmet>
+        <script defer data-domain="zhafranarman.com" src="https://analytics.zhafran.dev/js/script.js"></script>
+      </Helmet>
+      <BrowserRouter>
+        <App />
+        <SpeedInsights />
+        <Analytics />
+      </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>,
 )
